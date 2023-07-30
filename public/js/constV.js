586e82e0c81b4cca4ac07c6e5ae410ab93c3c61a
kynv <nvk.uet@gmail.com>
2023-07-30 12:48:28 +0700

    update const SFITT KR

--- public/js/constV.js
+++ public/js/constV.js
@@ -2,5 +2,5 @@
 const SMLN_MAXCHAP_KR = 664;
 const SMLN_MAXCHAP_EN = 421;
 //constant for Solo Farmer in the tower
-const SFITT_MAXCHAP_KR = 429;
+const SFITT_MAXCHAP_KR = 433;
 const SFITT_MAXCHAP_EN = 97;
