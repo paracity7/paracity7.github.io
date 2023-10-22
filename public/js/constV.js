622a598c40ba3af04713e65cd572e573aea2e287
kynv <nvk.uet@gmail.com>
2023-10-22 21:25:23 +0700

    add chap 486-529 sfitt kr

--- public/js/constV.js
+++ public/js/constV.js
@@ -2,5 +2,5 @@
 const SMLN_MAXCHAP_KR = 664;
 const SMLN_MAXCHAP_EN = 421;
 //constant for Solo Farmer in the tower
-const SFITT_MAXCHAP_KR = 485;
+const SFITT_MAXCHAP_KR = 529;
 const SFITT_MAXCHAP_EN = 127;
