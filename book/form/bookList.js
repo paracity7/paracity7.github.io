8c5d9c4c2869201c038f3120be01dd5230d6ee80
paracity7 <137040505+paracity7@users.noreply.github.com>
2024-05-07 12:40:40 +0900

    mod: value with price

---
+++ book/form/bookList.js
@@ -0,0 +1,51 @@
+const book25k = [
+    "Boruto: Naruto hậu sinh khả uý", 
+    "Iruma giá đáo",
+    "Học viện siêu anh hùng",
+    "Dr Stone",
+    "Thiên thần diệt thế",
+    "One Punch Man (Manga)",
+    "One Piece",
+    "Conan",
+    "Black Clover"
+];
+
+const book30k = [
+    "Học viện siêu anh hùng Team Up Mission"
+];
+
+const book35k = [
+    "World Trigger"
+];
+
+const book40k = [
+    "Bocchi The Rock",
+    "Chúa tể bóng tối (Manga)",
+    "Chu du ẩm thực tại dị giới với kĩ năng không tưởng",
+    "Nhóc Maruko"
+];
+
+const book45k = [
+    "Friezen: Pháp sư tiễn táng"
+];
+
+const book48k = [
+    "Attack on Titan"
+];
+
+const book50k = [
+    "Thiên sứ nhà bên (Manga)"
+];
+
+const book75k = [
+    "Dragon ball SD 7 Viên ngọc rồng nhí"
+];
+
+
+const book88k = [
+    "Solo Leveling: Tôi thăng cấp một mình (Manga)"
+];
+
+const book95k = [
+    "Soul Eater (bản NXB Trẻ)"
+];
